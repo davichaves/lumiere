e48d8bf4060c52740d7304fbfa64242c5d7fcc81
Davi Chaves <dchaves@usc.edu>
2020-05-13 21:02:22 -0700

    Updates

---
+++ precache-manifest.689381a7cb7d523f37482badfa77403e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250f078526793f4b1dd7c8eefda38215",
+    "url": "/lumi/index.html"
+  },
+  {
+    "revision": "dc4ddb2dd6175c117299",
+    "url": "/lumi/static/css/2.d54bb455.chunk.css"
+  },
+  {
+    "revision": "dc4ddb2dd6175c117299",
+    "url": "/lumi/static/js/2.a04d67e1.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/lumi/static/js/2.a04d67e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2a61ebd65a8ce286f4",
+    "url": "/lumi/static/js/3.770ac747.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/lumi/static/js/3.770ac747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a2092d90aea96f77ae",
+    "url": "/lumi/static/js/4.82194818.chunk.js"
+  },
+  {
+    "revision": "41ed2287cf12e24f4a31",
+    "url": "/lumi/static/js/5.3521a136.chunk.js"
+  },
+  {
+    "revision": "074bf116fa5a8a9baefe",
+    "url": "/lumi/static/js/main.08ece5a9.chunk.js"
+  },
+  {
+    "revision": "ae92c2b385cf58f0ec1a",
+    "url": "/lumi/static/js/runtime-main.f06ffdce.js"
+  }
+]);
